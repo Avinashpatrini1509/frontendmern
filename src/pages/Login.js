faa3cfd85143814db4c5ac361ad1b8d614623fdf
Avinashpatrini1509 <avinashpatrini@gmail.com>
2024-07-17 19:48:20 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
     const submitHandler =async(values) => {
         try{
             setLoading(true)
-          const {data} = await axios.post('/users/login',values)
+          const {data} = await axios.post('https://backendmern-ds68.onrender.com/users/login',values)
           setLoading(false)
           message.success("login success")
           localStorage.setItem('user',JSON.stringify({...data,password:''}))
